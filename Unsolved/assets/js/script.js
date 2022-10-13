b163c638c8ff0e1c5a4224aea7b34a4d1d94917e
Tom Bond <tombond248@gmail.com>
2022-10-13 20:02:57 +0100

    complete

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -1,11 +1,18 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
+  document.querySelector("#key").innerHTML = event.key
+  document.querySelector("#code").innerHTML = event.code
+  document.querySelector("#status").innerHTML = "KEYDOWN Event"
 }
 
 function keyupAction() {
+  document.querySelector("#key").innerHTML = ""
+  document.querySelector("#code").innerHTML = ""
   document.querySelector("#status").innerHTML = "KEYUP Event";
 }
 
 document.addEventListener("keyup", keyupAction);
+
+document.addEventListener("keydown", keydownAction);
 // TODO: Add Event Listener for "keydown" event
 
